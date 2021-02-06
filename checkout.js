0c496766a424301cd4326e7301d4cbf675a3520e
pushpal chakrabarty <pushpalchakrabarty99@gmail.com>
2021-02-06 08:31:22 +0530

    navbar addition & other fixes

--- checkout.js
+++ checkout.js
@@ -5,13 +5,16 @@ const queryFun = () => {
 }
 const loadCartData = () => {
     let id = queryFun()
-    url = `http://localhost:3000/cart?${id}`
+    url = `http://localhost:3000/cart`
     console.log(url)
     fetch(url).then((data) => data.json()).then((data) => createCart(data))
 }
 
 const createCart = (data) => {
-    totalAmt = data[0].current_price * data[0].qty;
+    let totalAmt = 0;
+    let deliveryChg = 0
+    for(i in data){
+    totalAmt += data[i].current_price * data[i].qty;
     let deliveryChg = 129;
     if (totalAmt>=1000){
         deliveryChg = 0;
@@ -21,14 +24,19 @@ const createCart = (data) => {
         document.querySelector('.shipping').innerHTML = `<p>Estimated delivery</p>
         <p>₹ 129</p>`
     }
-    console.log(data[0])
-    document.querySelector('.itemImg').innerHTML= `<img src="${data[0].img}"/>`
-    document.querySelector('.orderItemName').textContent = data[0].name
+    console.log(data[i])
+    if(data.length == 1){
+    document.querySelector('.itemImg').innerHTML= `<img src="${data[i].img}"/>`
+    document.querySelector('.orderItemName').textContent = data[i].name
     document.querySelector('.itemOrderDetail').innerHTML = `
-                            <p>Size: <strong></strong>${data[0].size.toUpperCase()}</p>
-                            <p>Quantity: <strong>${data[0].qty}</strong></p>
-                            <p><strong>₹ ${totalAmt}</strong></p>
+                            <p>Size:<strong>${data[i].size.toUpperCase()}</strong></p>
+                            <p>Quantity:<strong>${data[i].qty}</strong></p>
+                            <p><strong>₹${totalAmt}</strong></p>
     `
+    }else{
+        document.querySelector('.items').style.display = 'none';
+    }
+    }
     document.querySelector('.total').textContent = "₹ " +(totalAmt + deliveryChg);
 }
 window.addEventListener('load', loadCartData)
